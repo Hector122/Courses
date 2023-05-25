71bcdca3a22cfbd9937fac9d8cabc8321164b06f
Hector Castillo <hector@Hectors-MacBook-Pro.local>
2023-05-25 08:34:27 -0400

    Add my first compose

--- Android/Oreilly/MyFirstCompose/app/src/main/java/com/corps/myfirstcompose/MainActivity.kt
+++ Android/Oreilly/MyFirstCompose/app/src/main/java/com/corps/myfirstcompose/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.corps.myfirstcompose
 
-import android.graphics.Paint.Align
-import android.graphics.Paint.Style
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,13 +18,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
@@ -31,17 +31,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.corps.myfirstcompose.ui.theme.MyFirstComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -63,6 +63,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
+    val buttonClickableState = remember { mutableStateOf(false) }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -89,15 +90,93 @@ fun CreateBizCard() {
                 Divider()
                 CreateInfo()
                 Button(
-                    onClick = { Log.d("Clicked","click on Button") },
-                    shape = RoundedCornerShape(3.dp),
-                    colors = ButtonDefaults.buttonColors(Color.Magenta)
+                    onClick = {
+                        Log.i("test", "button in")
+                        buttonClickableState.value = !buttonClickableState.value
+                    },
+                    shape = RoundedCornerShape(3.dp)
                 ) {
-                    Text(text = "Portfolio")
+                    Text(
+                        text = "Portfolio",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+
+                // TODO: verify why I can see this content
+                if (buttonClickableState.value) {
+                    Content()
+                } else {
+                    Box {}
                 }
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+            Portfolio(
+                data = listOf(
+                    "Project 1",
+                    "Project 2",
+                    "Project 3",
+                    "Project 4",
+                    "Project 5"
+                )
+            )
+        }
+
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    //create a scrollable list.
+    LazyColumn {
+        items(data) { item ->
+            Card(
+                Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                shape = RectangleShape,
+                elevation = CardDefaults.cardElevation(4.dp)
+            ) {
+                Row(
+                    Modifier
+                        .padding(8.dp)
+                        .background(MaterialTheme.colorScheme.surface)
+                        .padding(16.dp)
+                ) {
+                    CreateImageProfile(modifier = Modifier.size(50.dp))
+                    Column(
+                        modifier = Modifier
+                            .padding(7.dp)
+                            .align(alignment = Alignment.CenterVertically)
+                    ) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "A great project", style = MaterialTheme.typography.bodySmall)
+                    }
+                }
+            }
+        }
+    }
+
 }
 
 @Composable
@@ -124,7 +203,7 @@ private fun CreateImageProfile(modifier: Modifier = Modifier) {
     Surface(
         modifier = Modifier
             .size(150.dp)
-            .padding(20.dp),
+            .padding(15.dp),
         shape = CircleShape,
         border = BorderStroke(0.5.dp, Color.LightGray),
         shadowElevation = 4.dp,
@@ -134,7 +213,7 @@ private fun CreateImageProfile(modifier: Modifier = Modifier) {
         Image(
             painter = painterResource(id = R.drawable.profile_image),
             contentDescription = "profile image",
-            modifier = Modifier.size(135.dp),
+            modifier = modifier.size(135.dp),
             contentScale = ContentScale.Crop
         )
 

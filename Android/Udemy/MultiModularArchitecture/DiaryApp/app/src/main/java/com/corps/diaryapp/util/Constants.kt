7e2f6b0e244c87585d18b16d8430b92744b0e96e
Hector Castillo <hector122@gmail.com>
2024-10-10 09:24:37 -0400

    remove secret from commit.

--- Android/Udemy/MultiModularArchitecture/DiaryApp/app/src/main/java/com/corps/diaryapp/util/Constants.kt
+++ Android/Udemy/MultiModularArchitecture/DiaryApp/app/src/main/java/com/corps/diaryapp/util/Constants.kt
@@ -1,18 +1,6 @@
 package com.corps.diaryapp.util
 
 object Constants {
-    const val APP_ID = ""
-    const val USER_ID = "hector122"
-    const val PASSWORD = "XCpBnRJM5npusBGQ"
-    const val CLIENT_ID = "182428938803-km6cg0ffqjaedql38g3481nnhrh9t6vk.apps.googleusercontent.com"
-    const val CLIENT_SECRET = "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"
+
 }
 
-//Mongo DB.
-//XCpBnRJM5npusBGQ
-//hector122
-
-
-
-//182428938803-km6cg0ffqjaedql38g3481nnhrh9t6vk.apps.googleusercontent.com
-//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

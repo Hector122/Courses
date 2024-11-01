fb79a25d73ad1b128c5e45e1cdf7194436f69dcf
Hector Castillo <hector122@gmail.com>
2024-11-01 10:40:08 -0400

    react-course use state.

---
+++ Web/CodingInFlow/react-beginner-2024/src/components/AddTodoForm.tsx
@@ -0,0 +1,11 @@
+export default function AddTodoForm() {
+  return (
+    <form className="flex">
+      <input
+        placeholder="Add a new todo"
+        className="rounded-s-md grow border border-gray-400j p-2"
+        type="text"
+      />
+    </form>
+  );
+}
---
+++ Web/CodingInFlow/react-beginner-2024/src/data/todos.ts
@@ -0,0 +1,20 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: 'John Doe 😍',
+        completed: false,
+        email: 'john@example.com'
+    },
+    { 
+        id: 2,
+        title: 'Jane Smith 👩‍💻', 
+        completed: false, 
+        email: 'jane@example.com' 
+    },
+    { id: 3, title: 'Bob Johnson 🚀',
+        completed: false, 
+        email: 'bob@example.com' },
+]
+

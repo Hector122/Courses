7de3541ba17a0b75bc78fe9ffce758b0460b6662
Hector Castillo <h.castillo@gbh.tech>
2025-03-27 11:31:04 -0400

    Add save meal method

--- Web/05-onwards-foodies-starting-project 2/components/meals/image-picker.js
+++ Web/05-onwards-foodies-starting-project 2/components/meals/image-picker.js
@@ -1,18 +1,43 @@
 "use client";
-import { use, useRef } from "react";
+import { use, useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 
 export default function ImagePicker({ label, name }) {
+  const [pickImage, setPickImage] = useState();
   const imagePicker = useRef();
 
-  function handleImageChange() {
+  function handlePickClick() {
     imagePicker.current.click();
+  }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
   }
 
   return (
     <div className={classes.picker}>
       <label htmlFor="image">{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickImage && <p>No image picked yet.</p>}
+          {pickImage && (
+            <Image src={pickImage} alt="The image selected by the user" fill />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -20,11 +45,13 @@ export default function ImagePicker({ label, name }) {
           name={name}
           accept=".jpg,.jpeg,.png"
           ref={imagePicker}
+          onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
           type="button"
-          onClick={handleImageChange}
+          onClick={handlePickClick}
         >
           Pick an Image
         </button>
--- Web/05-onwards-foodies-starting-project 2/lib/meals.js
+++ Web/05-onwards-foodies-starting-project 2/lib/meals.js
@@ -1,4 +1,6 @@
 import sql from "better-sqlite3";
+import slugify from "slugify";
+import xss from "xss";
 
 const db = sql("meals.db");
 
@@ -9,4 +11,12 @@ export async function getMeals() {
 
 export function getMeal(slug) {
   return db.prepare("SELECT * FROM meals WHERE slug = ? ").get(slug);
+}
+
+export async function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.title = xss(meal.title);
+  meal.summary = xss(meal.summary);
+  meal.instructions = xss(meal.instructions);
+
 }

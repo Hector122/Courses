6220d0e9c605a44c7c836f833a46ed1c2bc877f2
Hector Castillo <h.castillo@gbh.tech>
2025-04-03 14:36:20 -0400

    Add validation field

--- Web/05-onwards-foodies-starting-project 2/app/meals/share/page.js
+++ Web/05-onwards-foodies-starting-project 2/app/meals/share/page.js
@@ -1,8 +1,18 @@
+'use client';
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/action";
+import MealsFormSummit from "@/components/meals/mealFormSummit";
+import { useFormState } from "react-dom";
+// import { useActionState } from "react";
+
 
 export default function ShareMealPage() {
+  // const [state, formAction] = useActionState(shareMeal, {
+  const [state, formAction] = useFormState(shareMeal, {
+    message: null,
+  });
+
   return (
     <>
       <header className={classes.header}>
@@ -12,7 +22,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -41,8 +51,9 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Meal Image" name="image" id="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSummit />
           </p>
         </form>
       </main>
